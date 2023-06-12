b6149ae75239b9a6637ac9c9e88c79ca9ab47978
Illarion Kopeika <larrypearson@yandex.ru>
2023-06-12 17:57:00 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 Region.create(name: 'Asia', image: 'https://www.pngplay.com/wp-content/uploads/6/Asia-Map-Gradient-PNG.png')
 Region.create(name: 'Europe', image: 'https://e7.pngegg.com/pngimages/810/128/png-clipart-united-kingdom-map-globe-europe-world-map.png')
+Region.create(name: 'Africa', image: 'https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663136566_2-mykaleidoscope-ru-p-afrika-kontinent-pinterest-2.jpg')
