e517803cc6f628ce932a057d5da5048041cbd286
Illarion Kopeika <larrypearson@yandex.ru>
2023-06-12 15:31:34 +0800

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Region.create(name: 'Asia', image: 'https://www.pngplay.com/wp-content/uploads/6/Asia-Map-Gradient-PNG.png')
+Region.create(name: 'Europe', image: 'https://e7.pngegg.com/pngimages/810/128/png-clipart-united-kingdom-map-globe-europe-world-map.png')
