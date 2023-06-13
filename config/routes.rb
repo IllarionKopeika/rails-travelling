aa08fa333c4b8f29578be1ff6d2fa6d1de935c9d
Illarion Kopeika <larrypearson@yandex.ru>
2023-06-13 16:29:58 +0800

    Add show page design

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'regions#index'
   get 'regions', to: 'regions#index'
   get 'regions/:id', to: 'regions#show'
 end
