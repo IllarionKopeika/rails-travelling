761aa21487280c2606fbf2b2db1b212998b1436b
Illarion Kopeika <larrypearson@yandex.ru>
2023-06-13 14:54:54 +0800

    some changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,17 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# windows-style images
 Region.create(name: 'Europe', image: 'https://img5.goodfon.ru/original/1024x768/2/5c/abstraktsiia-windows-evropa-chast-sveta.jpg')
 Region.create(name: 'Asia', image: 'https://img5.goodfon.ru/original/640x480/0/33/abstraktsiia-windows-chast-sveta-aziia.jpg')
+Region.create(name: 'Africa', image: 'https://img5.goodfon.ru/original/640x480/b/4b/afrika-kontinent-abstraktsiia-windows.jpg')
+Region.create(name: 'Australia', image: 'https://img5.goodfon.com/original/2880x900/1/27/abstraktsiia-windows-kontinent-avstraliia.jpg')
+Region.create(name: 'North America', image: 'https://img5.goodfon.com/original/800x480/8/21/abstraktsiia-windows-kontinent-severnaia-amerika.jpg')
+Region.create(name: 'South America', image: 'https://img5.goodfon.ru/original/640x480/a/b8/abstraktsiia-windows-kontinent-iuzhnaia-amerika.jpg')
+
+# gradient-style images
+Region.create(name: 'Europe', image: 'https://thelawyer.imgix.net/content/uploads/2020/05/12130717/shutterstock_1074619370-scaled.jpg')
+Region.create(name: 'Asia', image: 'https://www.pngplay.com/wp-content/uploads/6/Asia-Map-Gradient-PNG.png')
 Region.create(name: 'Africa', image: 'https://img5.goodfon.ru/original/640x480/b/4b/afrika-kontinent-abstraktsiia-windows.jpg')
 Region.create(name: 'Australia', image: 'https://img5.goodfon.com/original/2880x900/1/27/abstraktsiia-windows-kontinent-avstraliia.jpg')
 Region.create(name: 'North America', image: 'https://img5.goodfon.com/original/800x480/8/21/abstraktsiia-windows-kontinent-severnaia-amerika.jpg')
